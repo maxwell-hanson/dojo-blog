923da8763787bd2c1d4140b03e93cf256c6237a6
Maxwell Hanson <m47hanson47@gmail.com>
2023-06-28 15:36:22 -0400

    Fixed growing array to reset upon new clicks

--- src/components/GetPhotos.jsx
+++ src/components/GetPhotos.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
+let photoArray = [];
+let photoObjArray = [];
+
 const placeDetailsAPI = {
-  base: "https://maps.googleapis.com/maps/api/place/details/json?",
+  base: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?",
   key: "AIzaSyA-ZQxK_Ewv79maROwoMONM5X2RykvAObA",
   place: "ChIJETbhTW8f54kRp3YOV7BgSMY",
 };
 
+
+
+
 const getPlaceDetails = (placeID) => {
+  photoObjArray.length = 0;
   console.log("fetching place details with placeID of:" + placeID);
 
   fetch(
@@ -14,24 +21,34 @@ const getPlaceDetails = (placeID) => {
   )
     .then((result) => result.json())
     .then((result) => {
-      console.log(result);
+      photoObjArray = result.result.photos;
+      console.log(photoObjArray);
+      extractPhotoRefs(photoObjArray);
     });
-
-  // fetch(`${weatherAPI.base}zip=${search}&appid=${weatherAPI.key}`)
-  //     .then((result) => result.json())
-  //     .then((result) => {
-  //       console.log(result);
 };
 
+
+
+
+const extractPhotoRefs = (array) => {
+  photoArray.length = 0;
+  for (const photoObjects of array) {
+    photoArray.push(photoObjects.photo_reference);
+  }
+  // console.log(photoArray);
+  console.log(photoArray[0]);
+};
+
+
+
+
+
+
 const GetPhotos = ({ placeID, userClickedPlace }) => {
-  // placeDetailsAPI.place = placeID;
+  placeDetailsAPI.place = placeID;
 
   let photoButton = null;
   if (userClickedPlace) {
-    //WE WANT PHOTOBUTTON TO BECOME THE PHOTOS THAT WE DISPLAY ONLY WHEN userClickedPlace is true!!!!!!!!!!!!
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // !!!!!!!!!!!!!
-
     photoButton = (
       <button onClick={() => getPlaceDetails(placeID)}>
         Click for photos!!!
